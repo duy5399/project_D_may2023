e1bb8adf0eb40f34a5127c063df7446e1df0cb86
Duy Le <duy5399@gmail.com>
2023-05-11 22:17:58 +0700

    11/05/2023 - lsd

---
+++ Assets/Scripts/map/ant-cave/MobController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobController : MonoBehaviour
+{
+    [SerializeField]
+    //public Rigidbody2D rb2d;
+    public Animator anim;
+    public GameObject target;
+    public float moveSpeed;
+    public bool facingLeft = true;
+    public float attackRange = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        //UpdateMovement();
+        ModFlip();
+        MobAttack();
+    }
+
+    public void UpdateMovement()
+    {
+        Vector2 targetPosition = new Vector2(target.transform.position.x, this.transform.position.y);
+        this.transform.position = Vector2.MoveTowards(this.transform.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void ModFlip()
+    {
+        if(this.transform.position.x > target.transform.position.x && !facingLeft)
+        {
+            this.transform.localScale = new Vector3(1, this.transform.localScale.y, this.transform.localScale.z);
+            facingLeft = true;
+        }
+        else if(this.transform.position.x < target.transform.position.x && facingLeft)
+        {
+            this.transform.localScale = new Vector3(-1, this.transform.localScale.y, this.transform.localScale.z);
+            facingLeft = false;
+        }
+    }
+
+    public void MobAttack()
+    {
+        if (Vector2.Distance(this.transform.position, target.transform.position) <= attackRange)
+        {
+            anim.SetBool("attack",true);
+        }
+        else
+            anim.SetBool("attack", false);
+    }
+}
